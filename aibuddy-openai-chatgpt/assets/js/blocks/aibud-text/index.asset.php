d5e8e8fbabdddc7948fea73d07f94f3d2280aa5d
Sean Murphy <sean@iamseanmurphy.com>
2025-04-07 20:17:58 +0000

    1.8.3

--- aibuddy-openai-chatgpt/assets/js/blocks/aibud-text/index.asset.php
+++ aibuddy-openai-chatgpt/assets/js/blocks/aibud-text/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c16c50e1361a27776f7c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a39adab6bd31d187649e');
